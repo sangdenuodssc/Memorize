d0bd2b4e9b98ac08ab107aa203e2df62e080ca7f
sangdenuodssc <16622045+sangdenuodssc@users.noreply.github.com>
2023-09-25 21:45:24 -0700

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Qiang Fu on 9/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.orange)
+        .imageScale(.large)
+        .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    //.strokeBorder(style: StrokeStyle(lineWidth: 10, dash: [10,2]))
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+            
+        })
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Qiang Fu on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
